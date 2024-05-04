ff3712cd9491ce9521bed6892935000b1ed13bed
shay <shayzhu@126.com>
2024-05-05 00:35:32 +0800

    add menu

--- scripts/main.sh
+++ scripts/main.sh
@@ -4,4 +4,19 @@
 source ./common.sh
 source ./reset.sh
 
-resetMain
+MENUS="初始化系统 退出"
+PS3="选择(1-6):"
+select MENU in $MENUS; do
+    case "${REPLY}" in
+        1)
+            resetMain
+            ;;
+        2)
+            break
+            ;;
+        *)
+            echo "不存在的操作，重新选择"
+        ;;
+    esac
+    
+done
