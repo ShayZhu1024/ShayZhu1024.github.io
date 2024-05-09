825bf54c0e821e5ef13e68996c7979b31c6275f9
shay <shayzhu@126.com>
2024-05-09 22:02:03 +0800

    add script

--- scripts/nginx-install/config.sh
+++ scripts/nginx-install/config.sh
@@ -3,9 +3,7 @@
 set -u
 set -e
 
-DEST=/app/nginx
-VERSION=1.20.2
-SRC=/usr/local/src
-TAR=/usr/local/src/nginx-1.20.2.tar.gz
-SRC_CODE=/usr/local/src/nginx-1.20.2
-ARG=--with-http_ssl_module
+DEST=/app/nginx/     #nginx的安装位置
+VERSION=1.20.2      #nginx的版本
+SRC=/usr/local/src/  #nginx的压缩包存放位置
+ARG=--with-http_ssl_module  #./configure 的配置参数
