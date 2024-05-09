5535c031b5e05c2ddbe0462ae2a850ac03827190
shay <shayzhu@126.com>
2024-05-09 21:46:07 +0800

    软件安装

--- scripts/nginx-install/nginx_install.sh
+++ scripts/nginx-install/nginx_install.sh
@@ -3,29 +3,15 @@
 set -u
 set -e
 
-if (($# < 2)) && [ ! -e ./config.sh ]; then 
-    echo -e "需要参数: 1:安装位置  2:版本号   [configureArg1...] or exits  ./config.sh"
-    exit 1 
-fi
-
 source ./load_dependencies.sh
+source ./config.sh
 
-if [ -e ./config.sh ]; then
-    source ./config.sh
-else
-
-    DEST=$1
-    VERSION=$2
-    SRC=/usr/local/src
-    TAR=/usr/local/src/nginx-${VERSION}.tar.gz
-    SRC_CODE=/usr/local/src/nginx-${VERSION}
-
-fi
+TAR=${SRC}/nginx-${VERSION}.tar.gz 
+SRC_CODE=${SRC}nginx-${VERSION}
 
 [ -e "$TAR" ] && { rm -rf "$TAR";  }
 
 wget https://nginx.org/download/nginx-${VERSION}.tar.gz -P  "$SRC"
-
 
 [ -e "$SRC_CODE" ] && { rm -rf "$SRC_CODE";  }
 
@@ -33,19 +19,6 @@ tar -xvf "$TAR"   -C  "$SRC"
 
 cd "$SRC_CODE"
 
-if [ ! -e ./config.sh ]; then
-    ARG="--with-http_ssl_module"
-fi
-
-if (($# >= 3)) && [ ! -e ./config.sh ]; then
-    shift 3
-    ARG=""
-    for i; do 
-        ARG+="$i  "
-    done
-fi
-
-
- ./configure  --prefix="$DEST"  $ARG  && make -j `egrep -c processor /proc/cpuinfo` && make install  
+./configure  --prefix="$DEST"  $ARG  && make -j `egrep -c processor /proc/cpuinfo` && make install  
 
 echo "make nginx and make install nginx successed !!!"
---
+++ scripts/nginx-install/readme.md
@@ -0,0 +1,19 @@
+# nginx 源码编译安装脚本
+
+```bash
+cd  ./nginx-install
+bash nginx_install.sh
+
+自定义配置，修改./config.sh
+
+
+DEST=/app/nginx     #nginx的安装位置
+VERSION=1.20.2      #nginx的版本
+SRC=/usr/local/src  #nginx的压缩包存放位置
+ARG=--with-http_ssl_module  #./configure 的配置参数
+
+
+
+
+对应需要的依赖包写在load_dependencies.sh 文件中
+```
