3d3aa6a36b10a3a5fc5c063725455bfe59ccd672
shay <shayzhu@126.com>
2024-05-19 22:18:45 +0800

    yum仓库脚本

---
+++ scripts/config-yum-server/config-yum-server.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+set -e
+set -u
+
+
+#前提配置好了远程yum源
+
+BASE_DIR=/var/www/html/linux/rockylinux/8/
+REPO_LIST="AppStream BaseOS epel extras"
+
+#安装http服务器
+yum install httpd -y
+systemctl  enable --now  httpd
+
+#创建好仓库基目录
+mkdir -p $BASE_DIR
+
+for repo in $REPO_LIST; do 
+    yum reposync --repoid="$repo"  --download-metadata  -p $BASE_DIR
+done
+
