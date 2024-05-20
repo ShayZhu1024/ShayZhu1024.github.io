bc2a762e23aff1c7d2ea2fa8b9f5ae4f2ac911f7
shay <shayzhu@126.com>
2024-05-20 18:36:44 +0800

    add script

--- scripts/config-yum-server/rsync-local-repo.sh
+++ scripts/config-yum-server/rsync-local-repo.sh
@@ -2,7 +2,8 @@
 
 BASE_DIR=/var/www/html/linux/
 
-yum install sshpass -y
+yum install sshpass httpd -y
+systemctl enable --now httpd
 
 sshpass -p 123456 scp -o StrictHostKeyChecking=no  -r  10.0.0.4:$BASE_DIR    $BASE_DIR  &>/dev/null
 
