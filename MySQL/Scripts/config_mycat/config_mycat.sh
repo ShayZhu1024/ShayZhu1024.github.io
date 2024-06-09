4b2b6ec08d7c9b858bd3e05052edf9d5b82c51c8
shay <shayzhu@126.com>
2024-06-09 18:00:17 +0800

    mysql

--- MySQL/Scripts/config_mycat/config_mycat.sh
+++ MySQL/Scripts/config_mycat/config_mycat.sh
@@ -7,7 +7,8 @@ set -e
 #需要在主服务器上创建mycat的连接管理账号
 #################注意#####################
 #mycat 端口和数据库设置可以在  ./schema.xml 和 ./server.xml 修改
-
+#注意：目标数据库上需要有对应的数据库比如：hellodb,没有会一直连接不上
+#
 
 MYCAT=Mycat-server-1.6.7.6-release-20210303094759-linux
 DIR=/apps/
